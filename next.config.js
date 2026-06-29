f87b181312bd3390e1de9feada0595dded4bfbaa
Aleks <alexpandov07@gmail.com>
2026-06-29 23:39:37 +0100

    Replace React proof page with finished static HTML, fix /proof conflict

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/proof', destination: '/proof.html' },
+    ];
+  },
+};
 
-export default nextConfig
+module.exports = nextConfig;
